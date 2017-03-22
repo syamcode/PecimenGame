f16a2010b472e0406b927446dcc2041aa5c31c6b
FADHIT\afadh <afadhitya@gmail.com>
2017-03-22 22:02:33 +0700

    add step

--- main.cpp
+++ main.cpp
@@ -17,25 +17,15 @@ int main()
 
     pacmanController peciman;
     initwindow(800, 600, "Pecimen Game");
-//    pacmanController peciman;
     CreateMap(level1, &peciman);
-    //DrawMap();
     char choose;
-    //peciman.direction = getch();
-    //printf("%d",peciman.direction);
-    //cleardevice();
-    //drawPacman(peciman);
-    //mainPeciman(peciman);
-//                        peciman.direction = RIGHT;
-//                    peciman.state = 1;
-    //int page = 1;
+    int step = 0;
 
     DrawMap();
     while (true)
     {
+        step++;
         begin = clock();
-        //setactivepage(page);
-        //setvisualpage(1 - page);
         if(kbhit())
         {
             choose = getch();
@@ -45,16 +35,17 @@ int main()
             case DOWNARROW :   peciman.direction = DOWN;break;
             case UPARROW :   peciman.direction = UP;break;
             }
-            //printf("%d", peciman.direction);
         }
-        //page = 1 - page;
-        Move(&peciman);
-        changeState(&peciman);
-        printf("%d %d\n",peciman.posX,peciman.posY);
-        //delay(100);
+        if (step%200 == 0){
+            printf("%d",step);
+            Move(&peciman);
+            changeState(&peciman);
+        }
+        printf("%d\n",step);
+       // printf("%d %d\n",peciman.posX,peciman.posY);
         end = clock();
-        time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-//        printf("Time = %f\n",time_spent);
+        time_spent = (double)(end - begin) / CLOCKS_PER_SEC; // Ulah di hapus
+//      printf("Time = %f\n",time_spent);
     }
 
     getch();
