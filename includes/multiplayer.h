d07e50e26f075557f8532822c3ff64a95eb62f67
CYBER-PEGASUS\Kurniawan <pega.kurniawan.02@gmail.com>
2017-05-23 21:58:09 +0700

    Add multyplayer

--- includes/multiplayer.h
+++ includes/multiplayer.h
@@ -1,24 +1,25 @@
-#ifndef GAME_H_INCLUDED
-#define GAME_H_INCLUDED
-#define UP2 119
-#define DOWN2 115
-#define LEFT2 97
-#define RIGHT2 100
+#ifndef MULTIPLAYER_H_INCLUDED
+#define MULTIPLAYER_H_INCLUDED
 
-#define GRIDSIZE2 20
-
-typedef struct{
-	int x;
-	int y;
-} position;
+#include "peciman.h"
+#define UPARROW2 119
+#define DOWNARROW2 115
+#define LEFTARROW2 97
+#define RIGHTARROW2 100
 
 typedef struct{
-    int direction;
-    int nextDirection;
-    position pos;
-    position initPos;
-    int state;
-}pacmanController;
+	char name[15];
+	int score;
+	int lives;
+	int foodCount;
+	int level;
+	pacmanController peciman;
+	ghostController ghost1;
+	ghostController ghost2;
+	ghostController ghost3;
+	ghostController ghost4;
+} playerControl1;
+
 
 int CanMovePeciman2(pacmanController peciman, int nextDirection);
 
